6be50a5c259458fde6438d5820f9417658cc0a2d
Nagano Kimiko <kimikonaganosw@gmail.com>
2017-02-12 19:51:27 +0900

    add_edit_btn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,9 +13,24 @@ class TweetsController < ApplicationController
     Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
   end
 
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
   def destroy
     tweet = Tweet.find(params[:id])
     tweet.destroy if tweet.user_id == current_user.id
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    if tweet.user_id == current_user.id
+      tweet.update(tweet_params)
+    end
   end
 
   private
